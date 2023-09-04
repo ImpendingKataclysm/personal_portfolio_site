cfdd5f63b0b5b95bb9437213c2420452d7a680f6
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-04 00:27:42 -0700

    Display portfolio projects from the database on the home page and include a link with each project to a detail page

--- main_site/urls.py
+++ main_site/urls.py
@@ -6,4 +6,6 @@ app_name = 'main_site'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('contact/', views.ContactView.as_view(), name='contact'),
+    path('portfolio/', views.PortfolioView.as_view(), name='portfolio'),
+    path('project/<slug:slug>', views.PortfolioDetailView.as_view(), name='portfolio_detail'),
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -13,12 +13,23 @@ class HomeView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         skills = models.Skill.objects.all()
         certificates = models.Certificate.objects.filter(is_active=True)
+        projects = models.PortfolioProject.objects.filter(is_active=True)
 
         context['skills'] = skills
         context['certificates'] = certificates
+        context['projects'] = projects
 
         return context
 
 
 class ContactView(generic.TemplateView):
     template_name = 'contact.html'
+
+
+class PortfolioView(generic.TemplateView):
+    template_name = 'portfolio.html'
+
+
+class PortfolioDetailView(generic.DetailView):
+    model = models.PortfolioProject
+    template_name = 'portfolio_detail.html'
