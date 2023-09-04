956cc112a8b4530f3b40363d55be18917db5ab63
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-03 23:06:43 -0700

    Display skills from the database on the home page

--- main_site/views.py
+++ main_site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from . import models
 
 
 class HomeView(generic.TemplateView):
@@ -7,6 +8,14 @@ class HomeView(generic.TemplateView):
     Display the home page
     """
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        skills = models.Skill.objects.all()
+
+        context['skills'] = skills
+
+        return context
 
 
 class ContactView(generic.TemplateView):
