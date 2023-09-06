b742226516794ed50f19533b74dc78e580fdf2c4
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-06 13:10:41 -0700

    Created a base template with a responsive nav bar and added some css.

--- project1/urls.py
+++ project1/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'rest_demo'
+app_name = 'project1'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('menu/', views.MenuView.as_view(), name='menu'),
+    path('reserve/', views.ReserveView.as_view(), name='reserve'),
 ]
--- project1/views.py
+++ project1/views.py
@@ -3,4 +3,20 @@ from django.views import generic
 
 
 class HomeView(generic.TemplateView):
-    template_name = 'home.html'
+    template_name = 'project1/home.html'
+
+
+class AboutView(generic.TemplateView):
+    template_name = 'project1/about.html'
+
+
+class ContactView(generic.TemplateView):
+    template_name = 'project1/contact.html'
+
+
+class MenuView(generic.TemplateView):
+    template_name = 'project1/menu.html'
+
+
+class ReserveView(generic.TemplateView):
+    template_name = 'project1/reserve.html'
