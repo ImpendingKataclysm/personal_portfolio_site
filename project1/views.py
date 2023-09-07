8259d44559cd254e9e3958a55985a18599f21158
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-07 10:58:03 -0700

    Display Menu Items from the database on the Menu page with a background image and responsive styles

--- project1/views.py
+++ project1/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
+from . import models
 
 
 class HomeView(generic.TemplateView):
+    """
+    Display Home Page
+    """
     template_name = 'project1/home.html'
 
 
 class AboutView(generic.TemplateView):
+    """
+    Display About Page
+    """
     template_name = 'project1/about.html'
 
 
@@ -14,8 +21,18 @@ class ContactView(generic.TemplateView):
     template_name = 'project1/contact.html'
 
 
-class MenuView(generic.TemplateView):
+class MenuView(generic.ListView):
+    """
+    Display the Menu Page with the Menu Items from the database sorted by type
+    """
+    queryset = models.MenuItem.objects.order_by('-date_added')
     template_name = 'project1/menu.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MenuView, self).get_context_data()
+        context['types'] = models.MENU_CATEGORIES
+
+        return context
 
 
 class ReserveView(generic.TemplateView):
